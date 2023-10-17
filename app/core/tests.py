cd545d08a5ebd7301d7d7c492eba14f879a9f6d5
MateusCampaner <mateus.campaner@edu.unifil.br>
2023-10-17 20:55:49 -0300

    teste

--- app/core/tests.py
+++ app/core/tests.py
@@ -3,40 +3,40 @@ from django.urls import reverse
 from .models import Pessoa
 
 class PessoaCrudTest(TestCase):
-    def test_salvar_view(self):
-        pessoa_teste = {'nome': 'Teste'}
+    def test_create_view(self):
+        pessoa_teste = {'nome': 'Mateus'}
 
-        response = self.client.post(reverse('salvar'), data=pessoa_teste)
+        response = self.client.post(reverse('Salvar'), data=pessoa_teste)
 
         self.assertEqual(response.status_code, 200)
 
         self.assertEqual(Pessoa.objects.count(), 1)
 
         pessoa_criada = Pessoa.objects.first()
-        self.assertEqual(pessoa_criada.nome, 'Teste')
+        self.assertEqual(pessoa_criada.nome, 'Mateus')
 
     def test_editar_view(self):
-        pessoa_teste = Pessoa.objects.create(nome='Teste')
+        pessoa_teste = Pessoa.objects.create(nome='Mateus')
         
-        response = self.client.get(reverse('editar', args=[pessoa_teste.id]))
+        response = self.client.get(reverse('Editar', args=[pessoa_teste.id]))
         
         self.assertEqual(response.status_code, 200)
         
         self.assertContains(response, 'Teste')
 
     def test_update_view(self):
-        pessoa_teste = Pessoa.objects.create(nome='Teste')
+        pessoa_teste = Pessoa.objects.create(nome='Mateus')
     
-        response = self.client.post(reverse('update', args=[pessoa_teste.id]), {'nome': 'NovoNome'})
+        response = self.client.post(reverse('update', args=[pessoa_teste.id]), {'nome': 'Eduardo'})
         
         self.assertEqual(response.status_code, 302) 
         self.assertRedirects(response, reverse('app'))
 
         pessoa_criada = Pessoa.objects.get(id=pessoa_teste.id)
-        self.assertEqual(pessoa_criada.nome, 'NovoNome')
+        self.assertEqual(pessoa_criada.nome, 'Eduardo')
 
     def test_delete_view(self):
-        pessoa_teste = Pessoa.objects.create(nome='Teste')
+        pessoa_teste = Pessoa.objects.create(nome='Mateus')
         
         response = self.client.post(reverse('delete', args=[pessoa_teste.id]))
         
